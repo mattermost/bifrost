74161bbf67bdeca889e875f311d3dafd9c26fbc3
Ian Whitlock <ian.whitlock@mattermost.com>
2021-04-26 13:19:53 -0500

    Test the case where there is a 204 returned instead of 200

--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -40,71 +40,110 @@ func TestHandler(t *testing.T) {
 		},
 	}
 
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// We test that the bucket name is stripped.
-		assert.Equal(t, "/foo", r.URL.Path)
+	{
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// We test that the bucket name is stripped.
+			assert.Equal(t, "/foo", r.URL.Path)
 
-		now := time.Now()
+			now := time.Now()
 
-		// Validate request headers
-		authHeader := r.Header.Get("Authorization")
-		date := r.Header.Get("X-Amz-Date")
+			// Validate request headers
+			authHeader := r.Header.Get("Authorization")
+			date := r.Header.Get("X-Amz-Date")
 
-		assert.True(t, strings.HasPrefix(authHeader, "AWS4-HMAC-SHA256"), "unexpected prefix for Authorization header: %s", authHeader)
-		assert.True(t, strings.HasPrefix(date, now.Format("20060102")), "unexpected prefix for X-Amz-Date header: %s", date)
+			assert.True(t, strings.HasPrefix(authHeader, "AWS4-HMAC-SHA256"), "unexpected prefix for Authorization header: %s", authHeader)
+			assert.True(t, strings.HasPrefix(date, now.Format("20060102")), "unexpected prefix for X-Amz-Date header: %s", date)
 
-		matches := regCred.FindStringSubmatch(authHeader)
-		require.Len(t, matches, 3, "unexpected number of matches")
-		assert.Equal(t, cfg.S3Settings.AccessKeyID, matches[1], "unexpected access key")
-		assert.Equal(t, now.Format("20060102"), matches[2], "unexpected date value")
+			matches := regCred.FindStringSubmatch(authHeader)
+			require.Len(t, matches, 3, "unexpected number of matches")
+			assert.Equal(t, cfg.S3Settings.AccessKeyID, matches[1], "unexpected access key")
+			assert.Equal(t, now.Format("20060102"), matches[2], "unexpected date value")
 
-		matches = regSign.FindStringSubmatch(authHeader)
-		require.Len(t, matches, 2, "unexpected number of matches")
+			matches = regSign.FindStringSubmatch(authHeader)
+			require.Len(t, matches, 2, "unexpected number of matches")
 
-		w.Header().Set("Content-Type", "application/xml")
-		w.Header().Set("Date", now.Format(time.RFC1123))
-		w.Header().Set("Last-Modified", now.Format(time.RFC1123))
-		w.Header().Set("Server", "Asgard")
-		w.Header().Set("X-Amz-Bucket-Region", cfg.S3Settings.Region)
-		w.Header().Set("X-Amz-Id-2", "id")
-		w.Header().Set("X-Amz-Request-Id", "reqId")
+			w.Header().Set("Content-Type", "application/xml")
+			w.Header().Set("Date", now.Format(time.RFC1123))
+			w.Header().Set("Last-Modified", now.Format(time.RFC1123))
+			w.Header().Set("Server", "Asgard")
+			w.Header().Set("X-Amz-Bucket-Region", cfg.S3Settings.Region)
+			w.Header().Set("X-Amz-Id-2", "id")
+			w.Header().Set("X-Amz-Request-Id", "reqId")
 
-		fmt.Fprintln(w, "Welcome to the realm eternal")
-	}))
-	defer ts.Close()
+			fmt.Fprintln(w, "Welcome to the realm eternal")
+		}))
+		defer ts.Close()
 
-	dummyGetHost := func(bucket, endPoint string) string {
-		return strings.TrimPrefix(ts.URL, "http://")
+		dummyGetHost := func(bucket, endPoint string) string {
+			return strings.TrimPrefix(ts.URL, "http://")
+		}
+
+		s := &Server{
+			logger:    mlog.NewTestingLogger(t, os.Stderr),
+			cfg:       cfg,
+			getHostFn: dummyGetHost,
+			client:    http.DefaultClient,
+			creds: credentials.NewStatic(cfg.S3Settings.AccessKeyID,
+				cfg.S3Settings.SecretAccessKey, "", credentials.SignatureV4),
+			metrics: newMetrics(),
+		}
+
+		req := httptest.NewRequest("GET", "http://example.com/"+cfg.S3Settings.Bucket+"/foo", nil)
+		w := httptest.NewRecorder()
+
+		s.handler()(w, req)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+		io.Copy(ioutil.Discard, resp.Body)
+
+		// Verify response headers
+		assert.Equal(t, http.StatusOK, resp.StatusCode, "unexpected status code")
+		assert.Equal(t, "application/xml", resp.Header.Get("Content-Type"), "unexpected content type")
+		assert.Equal(t, "Asgard", resp.Header.Get("Server"), "unexpected server")
+		assert.Equal(t, cfg.S3Settings.Region, resp.Header.Get("X-Amz-Bucket-Region"), "unexpected region")
+		assert.Equal(t, "id", resp.Header.Get("X-Amz-Id-2"), "unexpected id")
+		assert.Equal(t, "reqId", resp.Header.Get("X-Amz-Request-Id"), "unexpected request id")
+		assert.NotEmpty(t, resp.Header.Get("Date"), "empty date")
+		assert.NotEmpty(t, resp.Header.Get("Last-Modified"), "empty last-modified")
+	}
+	{
+		// test to see if a non-200 response code is handled correctly
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// We test that the bucket name is stripped.
+			assert.Equal(t, "/foo", r.URL.Path)
+			w.WriteHeader(204)
+			fmt.Fprintln(w, "Welcome to the realm eternal")
+		}))
+		defer ts.Close()
+
+		dummyGetHost := func(bucket, endPoint string) string {
+			return strings.TrimPrefix(ts.URL, "http://")
+		}
+
+		s := &Server{
+			logger:    mlog.NewTestingLogger(t, os.Stderr),
+			cfg:       cfg,
+			getHostFn: dummyGetHost,
+			client:    http.DefaultClient,
+			creds: credentials.NewStatic(cfg.S3Settings.AccessKeyID,
+				cfg.S3Settings.SecretAccessKey, "", credentials.SignatureV4),
+			metrics: newMetrics(),
+		}
+
+		req := httptest.NewRequest("GET", "http://example.com/"+cfg.S3Settings.Bucket+"/foo", nil)
+		w := httptest.NewRecorder()
+
+		s.handler()(w, req)
+
+		resp := w.Result()
+		defer resp.Body.Close()
+		io.Copy(ioutil.Discard, resp.Body)
+
+		// Verify response headers
+		assert.Equal(t, http.StatusNoContent, resp.StatusCode, "unexpected status code")
 	}
 
-	s := &Server{
-		logger:    mlog.NewTestingLogger(t, os.Stderr),
-		cfg:       cfg,
-		getHostFn: dummyGetHost,
-		client:    http.DefaultClient,
-		creds: credentials.NewStatic(cfg.S3Settings.AccessKeyID,
-			cfg.S3Settings.SecretAccessKey, "", credentials.SignatureV4),
-		metrics: newMetrics(),
-	}
-
-	req := httptest.NewRequest("GET", "http://example.com/"+cfg.S3Settings.Bucket+"/foo", nil)
-	w := httptest.NewRecorder()
-
-	s.handler()(w, req)
-
-	resp := w.Result()
-	defer resp.Body.Close()
-	io.Copy(ioutil.Discard, resp.Body)
-
-	// Verify response headers
-	assert.Equal(t, http.StatusOK, resp.StatusCode, "unexpected status code")
-	assert.Equal(t, "application/xml", resp.Header.Get("Content-Type"), "unexpected content type")
-	assert.Equal(t, "Asgard", resp.Header.Get("Server"), "unexpected server")
-	assert.Equal(t, cfg.S3Settings.Region, resp.Header.Get("X-Amz-Bucket-Region"), "unexpected region")
-	assert.Equal(t, "id", resp.Header.Get("X-Amz-Id-2"), "unexpected id")
-	assert.Equal(t, "reqId", resp.Header.Get("X-Amz-Request-Id"), "unexpected request id")
-	assert.NotEmpty(t, resp.Header.Get("Date"), "empty date")
-	assert.NotEmpty(t, resp.Header.Get("Last-Modified"), "empty last-modified")
 }
 
 func TestWriteError(t *testing.T) {
